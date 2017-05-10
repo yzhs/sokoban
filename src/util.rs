aaab5d4e0cff2cd536641c414717ddf47d98663f
Colin Benner <colin@yzhs.de>
2017-05-10 11:33:26 +0200

    Improve match statements in util.rs

--- src/util.rs
+++ src/util.rs
@@ -15,11 +15,11 @@ pub enum SokobanError {
 impl fmt::Display for SokobanError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use self::SokobanError::*;
-        match self {
-            &IoError(ref err) => write!(f, "{}", err),
-            &NoWorker(lvl) => write!(f, "NoWorker({})", lvl),
-            &TwoWorkers(lvl) => write!(f, "TwoWorkers({})", lvl),
-            &CratesGoalsMismatch(lvl, goals_minus_crates) => {
+        match *self {
+            IoError(ref err) => write!(f, "{}", err),
+            NoWorker(lvl) => write!(f, "NoWorker({})", lvl),
+            TwoWorkers(lvl) => write!(f, "TwoWorkers({})", lvl),
+            CratesGoalsMismatch(lvl, goals_minus_crates) => {
                 write!(f, "CratesGoalsMismatch({}, {})", lvl, goals_minus_crates)
             }
         }
@@ -30,11 +30,11 @@ impl Error for SokobanError {
     #[doc(hidden)]
     fn description(&self) -> &str {
         use self::SokobanError::*;
-        match self {
-            &IoError(ref err) => err.description(),
-            &TwoWorkers(_) => "More than one worker found.",
-            &NoWorker(_) => "No worker found.",
-            &CratesGoalsMismatch(_, _) => "The number of crates and goals does not match",
+        match *self {
+            IoError(ref err) => err.description(),
+            TwoWorkers(_) => "More than one worker found.",
+            NoWorker(_) => "No worker found.",
+            CratesGoalsMismatch(_, _) => "The number of crates and goals does not match",
         }
     }
 }
