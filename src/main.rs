548c9a097752498b5f4a18c531b3059b21623c47
Colin Benner <colin@yzhs.de>
2017-06-22 17:38:32 +0200

    Simplify main() and rendering code

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,7 @@ pub struct Gui {
     window_size: [u32; 2],
 
     textures: Textures,
+    background: Option<Texture2d>,
 
     /// Is the shift key currently pressed?
     shift_pressed: bool,
@@ -55,9 +56,6 @@ pub struct Gui {
 
     /// Current cursor position
     cursor_pos: [f64; 2],
-
-    /// Size of each cell
-    tile_size: i32,
 
     worker: Sprite,
     crates: Vec<Sprite>,
@@ -80,12 +78,12 @@ impl Gui {
 
             window_size: [640, 480],
             textures,
+            background: None,
 
             shift_pressed: false,
             control_pressed: false,
             cursor_pos: [0.0, 0.0],
 
-            tile_size: 50,
             worker,
             crates: vec![],
         };
@@ -137,12 +135,17 @@ impl Gui {
         }
         Nothing
     }
+    fn tile_size(&self) -> f64 {
+        let columns = self.game.columns() as u32;
+        let rows = self.game.rows() as u32;
+        min(self.window_size[0] / columns, self.window_size[1] / rows) as f64
+    }
 
     /// Handle a mouse click.
     fn click_to_command(&mut self, mouse_button: MouseButton) -> Command {
         let columns = self.game.columns() as isize;
         let rows = self.game.rows() as isize;
-        let tile_size = self.tile_size as f64;
+        let tile_size = self.tile_size();
 
         let (offset_x, offset_y) = if self.aspect_ratio() < 1.0 {
             ((self.window_size[0] as f64 - columns as f64 * tile_size) / 2.0, 0.0)
@@ -162,131 +165,133 @@ impl Gui {
 
 
     /// Render the static tiles of the current level onto a texture.
-    fn generate_background(&mut self, display: &Facade) -> Texture2d {
-        let columns = self.game.columns() as u32;
-        let rows = self.game.rows() as u32;
-        self.tile_size = min(self.window_size[0] / columns, self.window_size[1] / rows) as i32;
-        let width = self.tile_size as u32 * columns;
-        let height = self.tile_size as u32 * rows;
+    fn generate_background(&mut self, display: &Facade) {
+        let target;
 
-        let lvl = self.current_level();
+        {
+            let columns = self.game.columns() as u32;
+            let rows = self.game.rows() as u32;
+            let tile_size = self.tile_size();
+            let width = tile_size as u32 * columns;
+            let height = tile_size as u32 * rows;
 
-        // Find transitions between wall and non-wall tiles
-        let mut horizontal_wall_floor = vec![];
-        let mut horizontal_wall_empty = vec![];
-        let mut vertical_wall_floor = vec![];
-        let mut vertical_wall_empty = vec![];
+            let lvl = self.current_level();
 
-        let mut previous_cell = Background::Empty;
-        for (i, &cell) in lvl.background.iter().enumerate() {
-            use Background::*;
-            let pos = lvl.position(i);
-            if pos.x == 0 {
-                previous_cell = Background::Empty;
-            }
+            // Find transitions between wall and non-wall tiles
+            let mut horizontal_wall_floor = vec![];
+            let mut horizontal_wall_empty = vec![];
+            let mut vertical_wall_floor = vec![];
+            let mut vertical_wall_empty = vec![];
 
-            match (previous_cell, cell) {
-                (Empty, Wall) | (Wall, Empty) => vertical_wall_empty.push(pos),
-                (Wall, Wall) => (),
-                (_, Wall) | (Wall, _) => vertical_wall_floor.push(pos),
-                _ => (),
-            }
-
-            previous_cell = cell;
-
-            if cell != Wall {
-                continue;
-            }
-            if pos.x + 1 == columns as isize {
-                vertical_wall_empty.push(pos.right());
-            }
-
-            let above = pos.above();
-            let below = pos.below();
-
-            if lvl.is_interior(above) {
-                horizontal_wall_floor.push(pos);
-            } else if lvl.is_outside(above) {
-                horizontal_wall_empty.push(pos);
-            }
-
-            if lvl.is_interior(below) {
-                horizontal_wall_floor.push(below);
-            } else if lvl.is_outside(below) {
-                horizontal_wall_empty.push(below);
-            }
-        }
-
-        // Create texture
-        let target = glium::texture::Texture2d::empty(display, width * 2, height * 2).unwrap();
-        target.as_surface().clear_color(0.0, 0.0, 0.0, 1.0);
-
-        let program = glium::Program::from_source(display,
-                                                  texture::VERTEX_SHADER,
-                                                  texture::FRAGMENT_SHADER,
-                                                  None)
-                .unwrap();
-
-        let params = glium::DrawParameters {
-            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullCounterClockwise,
-            blend: glium::Blend::alpha_blending(),
-            ..Default::default()
-        };
-
-        // Render each of the (square) tiles
-        for &value in &[Background::Floor, Background::Goal, Background::Wall] {
-            let mut vertices = vec![];
+            let mut previous_cell = Background::Empty;
             for (i, &cell) in lvl.background.iter().enumerate() {
-                if cell != value {
+                use Background::*;
+                let pos = lvl.position(i);
+                if pos.x == 0 {
+                    previous_cell = Background::Empty;
+                }
+
+                match (previous_cell, cell) {
+                    (Empty, Wall) | (Wall, Empty) => vertical_wall_empty.push(pos),
+                    (_, Wall) | (Wall, _) => vertical_wall_floor.push(pos),
+                    _ => (),
+                }
+
+                previous_cell = cell;
+
+                if cell != Wall {
                     continue;
                 }
-                let pos = lvl.position(i);
-                vertices.extend(texture::create_quad_vertices(pos, columns, rows, 1.0));
-            }
-            let vertex_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
+                if pos.x + 1 == columns as isize {
+                    vertical_wall_empty.push(pos.right());
+                }
 
-            let texture = match value {
-                Background::Empty => continue,
-                Background::Floor => &self.textures.floor,
-                Background::Goal => &self.textures.goal,
-                Background::Wall => &self.textures.wall,
+                let above = pos.above();
+                let below = pos.below();
+
+                if lvl.is_interior(above) {
+                    horizontal_wall_floor.push(pos);
+                } else if lvl.is_outside(above) {
+                    horizontal_wall_empty.push(pos);
+                }
+
+                if lvl.is_interior(below) {
+                    horizontal_wall_floor.push(below);
+                } else if lvl.is_outside(below) {
+                    horizontal_wall_empty.push(below);
+                }
+            }
+
+            // Create texture
+            target = glium::texture::Texture2d::empty(display, width * 2, height * 2).unwrap();
+            target.as_surface().clear_color(0.0, 0.0, 0.0, 1.0);
+
+            let program = glium::Program::from_source(display,
+                                                      texture::VERTEX_SHADER,
+                                                      texture::FRAGMENT_SHADER,
+                                                      None)
+                    .unwrap();
+
+            let params = glium::DrawParameters {
+                backface_culling: glium::draw_parameters::BackfaceCullingMode::CullCounterClockwise,
+                blend: glium::Blend::alpha_blending(),
+                ..Default::default()
             };
-            let uniforms = uniform!{
+
+            // Render each of the (square) tiles
+            for &value in &[Background::Floor, Background::Goal, Background::Wall] {
+                let mut vertices = vec![];
+                for (i, &cell) in lvl.background.iter().enumerate() {
+                    if cell != value {
+                        continue;
+                    }
+                    let pos = lvl.position(i);
+                    vertices.extend(texture::create_quad_vertices(pos, columns, rows, 1.0));
+                }
+                let vertex_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
+
+                let texture = match value {
+                    Background::Empty => continue,
+                    Background::Floor => &self.textures.floor,
+                    Background::Goal => &self.textures.goal,
+                    Background::Wall => &self.textures.wall,
+                };
+                let uniforms = uniform!{
                 tex: texture,
             };
 
-            target
-                .as_surface()
-                .draw(&vertex_buffer, &NO_INDICES, &program, &uniforms, &params)
-                .unwrap();
-        }
-
-        // Render the transitions
-        let mut vertices = vec![];
-        let tex = &self.textures;
-        for &(ref positions, orientation, texture) in
-            [(horizontal_wall_empty, Direction::Up, &tex.transition_wall_empty_horizontal),
-             (horizontal_wall_floor, Direction::Up, &tex.transition_wall_floor_horizontal),
-             (vertical_wall_empty, Direction::Left, &tex.transition_wall_empty_vertical),
-             (vertical_wall_floor, Direction::Left, &tex.transition_wall_floor_vertical)]
-                    .into_iter() {
-
-            for &pos in positions {
-                vertices.extend(texture::create_transition(pos, columns, rows, orientation));
+                target
+                    .as_surface()
+                    .draw(&vertex_buffer, &NO_INDICES, &program, &uniforms, &params)
+                    .unwrap();
             }
-            let vertex_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
-            let uniforms = uniform!{
+
+            // Render the transitions
+            let mut vertices = vec![];
+            let tex = &self.textures;
+            for &(ref positions, orientation, texture) in
+                &[(horizontal_wall_empty, Direction::Up, &tex.transition_wall_empty_horizontal),
+                  (horizontal_wall_floor, Direction::Up, &tex.transition_wall_floor_horizontal),
+                  (vertical_wall_empty, Direction::Left, &tex.transition_wall_empty_vertical),
+                  (vertical_wall_floor, Direction::Left, &tex.transition_wall_floor_vertical)] {
+
+                for &pos in positions {
+                    vertices.extend(texture::create_transition(pos, columns, rows, orientation));
+                }
+                let vertex_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
+                let uniforms = uniform!{
                 tex: texture
             };
-            target
-                .as_surface()
-                .draw(&vertex_buffer, &NO_INDICES, &program, &uniforms, &params)
-                .unwrap();
+                target
+                    .as_surface()
+                    .draw(&vertex_buffer, &NO_INDICES, &program, &uniforms, &params)
+                    .unwrap();
 
-            vertices.clear();
+                vertices.clear();
+            }
         }
 
-        target
+        self.background = Some(target);
     }
 
     /// Create sprites for movable entities of the current level.
@@ -298,6 +303,8 @@ impl Gui {
             .map(|&pos| Sprite::new(pos, texture::TileKind::Crate))
             .collect();
         // TODO simplify hashmap -> iter -> vec -> iter -> vec -> iter -> vec
+
+        self.background = None;
     }
 
     /// Compute the window’s aspect ratio.
@@ -307,13 +314,19 @@ impl Gui {
         height as f32 / width as f32
     }
 
-    /// Render the current level. `bg` is the static part of the level.
-    fn render_level(&mut self, display: &GlutinFacade, bg: &Texture2d, font_data: &FontData) {
+    /// Render the current level.
+    fn render_level(&mut self, display: &GlutinFacade, font_data: &FontData) {
         let params = glium::DrawParameters {
             backface_culling: glium::draw_parameters::BackfaceCullingMode::CullCounterClockwise,
             blend: glium::Blend::alpha_blending(),
             ..Default::default()
         };
+
+        // Do we have to update the cache?
+        if self.background.is_none() {
+            self.generate_background(display);
+        }
+        let bg = self.background.as_ref().unwrap();
 
         // Draw background
         let vertices = texture::create_background_quad(self.aspect_ratio(),
@@ -457,6 +470,62 @@ impl Gui {
 
         target.finish().unwrap();
     }
+
+    pub fn handle_responses(&mut self, queue: &mut VecDeque<Response>) {
+        // Handle responses from the backend.
+        while let Some(response) = queue.pop_front() {
+            use Response::*;
+            match response {
+                LevelFinished(resp) => {
+                    if !self.level_solved {
+                        use save::UpdateResponse::*;
+                        self.level_solved = true;
+                        match resp {
+                            FirstTimeSolved => {
+                                info!("You have successfully solved this level for the first time! \
+                                   Congratulations!")
+                            }
+                            Update { moves, pushes } => {
+                                if moves && pushes {
+                                    info!("Your solution is the best so far, both in terms of moves and pushes!");
+                                } else if moves {
+                                    info!("Your solution is the best so far in terms of moves!");
+                                } else if pushes {
+                                    info!("Your solution is the best so far in terms of pushes!");
+                                } else {
+                                    info!("Solved the level without creating a new high score.")
+                                }
+                            }
+                        }
+                    }
+                }
+                ResetLevel | NewLevel(_) => {
+                    if let Response::NewLevel(rank) = response {
+                        info!("Loading level #{}", rank);
+                    }
+                    self.end_of_collection = false;
+                    self.level_solved = false;
+                    self.update_sprites();
+                }
+                MoveWorkerTo(pos, dir) => {
+                    self.worker.move_to(pos);
+                    self.worker.set_direction(dir);
+                    break;
+                }
+                MoveCrateTo(id, pos) => self.crates[id].move_to(pos),
+
+                // Errors
+                CannotMove(WithCrate(true), Obstacle::Wall) => info!("A crate hit a wall"),
+                CannotMove(WithCrate(false), Obstacle::Wall) => info!("The worker hit a wall"),
+                CannotMove(WithCrate(true), Obstacle::Crate) => info!("Two crates collided"),
+                CannotMove(WithCrate(false), Obstacle::Crate) => info!("The worker ran into a crate"),
+                NoPathfindingWhilePushing => error!("Path finding when moving crates is not implemented"),
+                NothingToUndo | Response::NothingToRedo => warn!("Cannot undo/redo move"),
+                NoPreviousLevel => warn!("Cannot go backwards past level 1"),
+                EndOfCollection => self.end_of_collection = true,
+            }
+        }
+    }
 }
 
 /// Map arrow keys to the corresponding directions, panic on other keys.
@@ -502,6 +571,10 @@ impl FontData {
 fn main() {
     use glium::DisplayBuild;
     const TITLE: &'static str = "Sokoban";
+
+    /*
+     * Initialization
+     */
 
     let matches = App::new(TITLE)
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -552,13 +625,14 @@ fn main() {
     let mut gui = Gui::new(&collection, Textures::new(&display));
     info!("Loading level #{}", gui.game.rank());
 
-    // Render static part of the level
-    let mut bg = gui.generate_background(&display);
-
     let mut queue = VecDeque::new();
 
+    /*
+     * Main loop
+     */
+
     loop {
-        gui.render_level(&display, &bg, &font_data);
+        gui.render_level(&display, &font_data);
 
         for ev in display.poll_events() {
             use glium::glutin::Event;
@@ -589,7 +663,7 @@ fn main() {
 
                 Event::Resized(w, h) => {
                     gui.window_size = [w, h];
-                    bg = gui.generate_background(&display);
+                    gui.background = None;
                 }
 
                 /*
@@ -605,60 +679,6 @@ fn main() {
             queue.extend(gui.game.execute(cmd));
         }
 
-
-        // Handle responses from the backend.
-        while let Some(response) = queue.pop_front() {
-            use Response::*;
-            match response {
-                LevelFinished(resp) => {
-                    if !gui.level_solved {
-                        gui.level_solved = true;
-                        use save::UpdateResponse::*;
-                        match resp {
-                            FirstTimeSolved => {
-                                info!("You have successfully solved this level for the first time! \
-                                   Congratulations!")
-                            }
-                            Update { moves, pushes } => {
-                                if moves && pushes {
-                                    info!("Your solution is the best so far, both in terms of moves and pushes!");
-                                } else if moves {
-                                    info!("Your solution is the best so far in terms of moves!");
-                                } else if pushes {
-                                    info!("Your solution is the best so far in terms of pushes!");
-                                } else {
-                                    info!("Solved the level without creating a new high score.")
-                                }
-                            }
-                        }
-                    }
-                }
-                ResetLevel | NewLevel(_) => {
-                    if let Response::NewLevel(rank) = response {
-                        info!("Loading level #{}", rank);
-                    }
-                    gui.end_of_collection = false;
-                    gui.level_solved = false;
-                    gui.update_sprites();
-                    bg = gui.generate_background(&display);
-                }
-                MoveWorkerTo(pos, dir) => {
-                    gui.worker.move_to(pos);
-                    gui.worker.set_direction(dir);
-                    break;
-                }
-                MoveCrateTo(id, pos) => gui.crates[id].move_to(pos),
-
-                // Errors
-                CannotMove(WithCrate(true), Obstacle::Wall) => info!("A crate hit a wall"),
-                CannotMove(WithCrate(false), Obstacle::Wall) => info!("The worker hit a wall"),
-                CannotMove(WithCrate(true), Obstacle::Crate) => info!("Two crates collided"),
-                CannotMove(WithCrate(false), Obstacle::Crate) => info!("The worker ran into a crate"),
-                NoPathfindingWhilePushing => error!("Path finding when moving crates is not implemented"),
-                NothingToUndo | Response::NothingToRedo => warn!("Cannot undo/redo move"),
-                NoPreviousLevel => warn!("Cannot go backwards past level 1"),
-                EndOfCollection => gui.end_of_collection = true,
-            }
-        }
+        gui.handle_responses(&mut queue);
     }
 }
