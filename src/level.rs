703594ed5b226e56ced386fd2043829d587d6078
Colin Benner <colin-github@yzhs.de>
2019-02-09 09:37:37 +0100

    Clean up `Level`, remove unused functions

--- src/level.rs
+++ src/level.rs
@@ -1,12 +1,7 @@
 pub mod builder;
 
-use std::{
-    collections::HashMap,
-    fmt,
-    sync::mpsc::Sender,
-};
+use std::{collections::HashMap, fmt, sync::mpsc::Sender};
 
-use crate::direction::*;
 use crate::event::Event;
 use crate::level::builder::{Foreground, LevelBuilder};
 use crate::move_::Move;
@@ -59,7 +54,7 @@ pub struct Level {
     pub listeners: Vec<Sender<Event>>,
 }
 
-/// Parse level and some basic utility functions. None of these change an existing `Level`. {{{
+/// Parse level and some basic utility functions. None of these change an existing `Level`.
 impl Level {
     /// Parse the ASCII representation of a level.
     pub fn parse(num: usize, string: &str) -> Result<Level, SokobanError> {
@@ -67,112 +62,11 @@ impl Level {
         Ok(builder.build())
     }
 
-    pub fn rank(&self) -> usize {
-        self.rank
-    }
-
-    pub fn rows(&self) -> usize {
-        self.rows
-    }
-
-    pub fn columns(&self) -> usize {
-        self.columns
-    }
-
-    pub fn worker_position(&self) -> Position {
-        self.worker_position
-    }
-
-    fn index(&self, pos: Position) -> usize {
-        pos.x as usize + pos.y as usize * self.columns()
-    }
-
-    pub fn position(&self, i: usize) -> Position {
-        Position::new(i % self.columns, i / self.columns)
-    }
-
-    pub fn background(&self, pos: Position) -> &Background {
-        &self.background[self.index(pos)]
-    }
-
-    fn in_bounds(&self, pos: Position) -> bool {
-        pos.x >= 0 && pos.y >= 0 && pos.x < self.columns() as isize && pos.y < self.rows() as isize
-    }
-
     /// Is there a crate at the given position?
     fn is_crate(&self, pos: Position) -> bool {
         self.crates.get(&pos).is_some()
     }
-
-    pub fn is_outside(&self, pos: Position) -> bool {
-        !self.in_bounds(pos) || *self.background(pos) == Background::Empty
-    }
-
-    /// The cell at the given position is neither empty, nor does it contain a wall.
-    pub fn is_interior(&self, pos: Position) -> bool {
-        use self::Background::*;
-
-        if !self.in_bounds(pos) {
-            return false;
-        }
-
-        match *self.background(pos) {
-            Floor | Goal => true,
-            _ => false,
-        }
-    }
-
-    /// Check whether the given level is completed, i.e. every goal has a crate on it, and every
-    /// crate is on a goal.
-    pub fn is_finished(&self) -> bool {
-        self.empty_goals == 0
-    }
-
-    /// How moves were performed to reach the current state?
-    pub fn number_of_moves(&self) -> usize {
-        self.number_of_moves
-    }
-
-    /// How many times have crates been moved to reach the current state?
-    pub fn number_of_pushes(&self) -> usize {
-        self.moves[0..self.number_of_moves]
-            .iter()
-            .filter(|x| x.moves_crate)
-            .count()
-    }
-
-    /// Which direction is the worker currently facing?
-    pub fn worker_direction(&self) -> Direction {
-        if self.number_of_moves == 0 {
-            Direction::Left
-        } else {
-            self.moves[self.number_of_moves - 1].direction
-        }
-    }
-
-    /// Create a string representation of the moves made to reach the current state.
-    pub fn moves_to_string(&self) -> String {
-        self.moves
-            .iter()
-            .take(self.number_of_moves)
-            .map(|mv| mv.to_char())
-            .collect()
-    }
-
-    /// Get an ordered list of the crates’ positions where the id of a crate is its index in the
-    /// list.
-    pub fn crate_positions(&self) -> Vec<Position> {
-        let mut crates: Vec<_> = self.crates.iter().collect();
-        crates.sort_by_key(|&(_pos, id)| id);
-        crates.into_iter().map(|(&pos, _id)| pos).collect()
-    }
-
-    pub fn background_cells(&self) -> &[Background] {
-        self.background.as_ref()
-    }
 }
-// }}}
-
 fn cell_to_char(background: Background, foreground: Foreground) -> char {
     match (background, foreground) {
         (Background::Wall, Foreground::None) => '#',
@@ -191,8 +85,8 @@ fn cell_to_char(background: Background, foreground: Foreground) -> char {
 
 impl fmt::Display for Level {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let columns = self.columns();
-        for i in 0..self.rows() {
+        let columns = self.columns;
+        for i in 0..self.rows {
             if i != 0 {
                 writeln!(f)?;
             }
@@ -211,6 +105,35 @@ impl fmt::Display for Level {
             }
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+impl Level {
+    fn index(&self, pos: Position) -> usize {
+        pos.x as usize + pos.y as usize * self.columns
+    }
+
+    fn background(&self, pos: Position) -> &Background {
+        &self.background[self.index(pos)]
+    }
+
+    fn in_bounds(&self, pos: Position) -> bool {
+        pos.x >= 0 && pos.y >= 0 && pos.x < self.columns as isize && pos.y < self.rows as isize
+    }
+
+    /// The cell at the given position is neither empty, nor does it contain a wall.
+    fn is_interior(&self, pos: Position) -> bool {
+        use self::Background::*;
+
+        if !self.in_bounds(pos) {
+            return false;
+        }
+
+        match *self.background(pos) {
+            Floor | Goal => true,
+            _ => false,
+        }
     }
 }
 
